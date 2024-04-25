b80de9520d6b2de14a0450f4f66ce72b1bf3157a
Marina <likholetova.marina@gmail.com>
2024-04-25 12:55:04 +0300

    add colors to logger

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const logger = require("./utils/logger")("main");
 
-logger.info("the script is running!");
+logger.info("info message");
+logger.warn("warn message");
+logger.error("error message");
--- utils/logger.js
+++ utils/logger.js
@@ -1,8 +1,10 @@
+const { bgBlue, bgYellow, bgRed} = require('colors/safe');
+
 function getLogger(moduleName) {
     return {
-        info: (...args) => console.log(`${moduleName}:`, ...args),
-        warn: (...args) => console.error(`${moduleName}:`, ...args),
-        error: (...args) => console.error(`${moduleName}:`, ...args),
+        info: (...args) => console.log(bgBlue(`${moduleName}:`), ...args),
+        warn: (...args) => console.error(bgYellow(`${moduleName}:`), ...args),
+        error: (...args) => console.error(bgRed(`${moduleName}:`), ...args),
     };
 }
 
