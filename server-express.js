799c0d9bf86399a11316035f78b2ee1bb19664eb
Marina <likholetova.marina@gmail.com>
2024-05-21 14:35:57 +0300

    integrate rotating-file-stream package

--- server-express.js
+++ server-express.js
@@ -5,14 +5,15 @@ const express = require("express");
 const morgan = require("morgan");
 
 const logger = require('./utils/logger')('express srv');
+const stream = require('./config/rotatingFileStreamConfig.js');
 
 const app = express();
 
 app.listen(srvConfig.port, () => logger.info(`server is listening on [${srvConfig.port}] port`));
 
-const accessLogger = morgan(':date :method :url :status');
+const accessLogger = morgan(':date :method :url :status', { stream });
 
-app.use(accessLogger)
+app.use(accessLogger);
 
 app.get('/healthcheck', (req, resp) => {
     resp.send('healthcheck passed!');
