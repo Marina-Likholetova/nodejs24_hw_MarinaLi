5347c51f84abc849889459940cc70c29ff44c5fd
Marina <likholetova.marina@gmail.com>
2024-05-21 13:47:06 +0300

    integrate morgan package

--- server-express.js
+++ server-express.js
@@ -2,12 +2,17 @@ require("dotenv").config();
 const { server: srvConfig } = require("config");
 
 const express = require("express");
+const morgan = require("morgan");
 
 const logger = require('./utils/logger')('express srv');
 
 const app = express();
 
 app.listen(srvConfig.port, () => logger.info(`server is listening on [${srvConfig.port}] port`));
+
+const accessLogger = morgan(':date :method :url :status');
+
+app.use(accessLogger)
 
 app.get('/healthcheck', (req, resp) => {
     resp.send('healthcheck passed!');
