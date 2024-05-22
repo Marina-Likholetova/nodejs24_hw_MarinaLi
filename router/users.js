ee3919c00d20a5c44c746ba8514ecb1a1142132c
Marina <likholetova.marina@gmail.com>
2024-05-22 11:19:47 +0300

    add validation to user routes

--- router/users.js
+++ router/users.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { StatusCodes } = require("http-status-codes");
+const { userDataValidator, userIdValidator } = require("../middlewares/validators/userValidators");
 
 const userRouter = Router();
 
@@ -7,15 +8,15 @@ userRouter.get("/", (_req, resp) => {
     resp.send([]);
 });
 
-userRouter.get("/:userId", (req, resp) => {
+userRouter.get("/:userId", userIdValidator, (req, resp) => {
     resp.send(req.params);
 });
 
-userRouter.post("/", (_req, resp) => {
+userRouter.post("/", userDataValidator, (_req, resp) => {
     resp.status(StatusCodes.CREATED).send("User created");
 });
 
-userRouter.delete("/:userId", (_req, resp) => {
+userRouter.delete("/:userId", userIdValidator, (_req, resp) => {
     resp.status(StatusCodes.NO_CONTENT).end();
 });
 
