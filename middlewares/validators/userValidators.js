3fcb7d036028cc7d3c2516364e6fdfd838d9fad4
Marina <likholetova.marina@gmail.com>
2024-05-22 12:26:17 +0300

    improve validation

--- middlewares/validators/userValidators.js
+++ middlewares/validators/userValidators.js
@@ -1,26 +1,32 @@
 const yup = require("yup");
 
 const userDataSchema = yup.object().shape({
-    username: yup.string().trim().required("Username is required").min(1),
-    email: yup.string().trim().email("Invalid email").required("Email is required"),
+    username: yup
+        .string()
+        .strict()
+        .typeError("should be a string")
+        .trim()
+        .min(1, "shoud be longer than 1 chart")
+        .required("required"),
+    email: yup.string().trim().email("Invalid email").required("required"),
 });
 
-const userIdSchema = yup.object().shape({
-    userId: yup
-        .string()
-        .trim()
-        .required("UserId is required")
-        .test("is-number", "UserId must be a positive integer", (value) => {
-            const parsed = Number(value);
-            return Number.isInteger(parsed) && parsed >= 0;
-        }),
-});
+const userIdSchema = yup
+        .number()
+        .typeError("must be a number")
+        .required("required")
+        .positive("must be a positive")
+        .integer("must be an integer")
 
 const userIdValidator = async (req, resp, next) => {
     try {
-        await userIdSchema.validate(req.params);
+        const parsed = await userIdSchema.validate(req.params.userId);
+        req.params.userId = parsed;
     } catch (err) {
-        resp.status(400).send({ error: err.message });
+        const errors = {
+            userId: err.message
+        };
+        resp.status(400).send({ error: errors });
         return;
     }
 
@@ -29,9 +35,18 @@ const userIdValidator = async (req, resp, next) => {
 
 const userDataValidator = async (req, resp, next) => {
     try {
-        await userDataSchema.validate(req.body);
+        await userDataSchema.validate(req.body, { abortEarly: false });
     } catch (err) {
-        resp.status(400).send({ error: err.message });
+        const errors = err.inner.reduce((acc, curr) => {
+            if (!acc[curr.path]) {
+                acc[curr.path] = [];
+            }
+
+            acc[curr.path].push(curr.message);
+            return acc;
+        }, {});
+
+        resp.status(400).send({ error: errors });
         return;
     }
 
