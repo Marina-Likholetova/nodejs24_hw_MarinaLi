d2c4dfef067cdbba3f9bc02e514181dfd9161afd
Marina <likholetova.marina@gmail.com>
2024-05-27 18:05:11 +0300

    add CORS setup

--- server-express.js
+++ server-express.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const { server: srvConfig, morgan: morganConfig } = require("config");
+const cors = require("cors");
 
 const express = require("express");
 const morgan = require("morgan");
 
-const logger = require('./utils/logger')('express srv');
-const stream = require('./config/rotatingFileStreamConfig.js');
+const logger = require("./utils/logger")("express srv");
+const stream = require("./config/rotatingFileStreamConfig.js");
 const { userRouter } = require("./router/users.js");
 const jsonBodyParser = express.json();
 
@@ -17,4 +18,4 @@ app.use(jsonBodyParser);
 
 app.use(morgan(morganConfig.format, { stream }));
 
-app.use('/users', userRouter);
+app.use("/users", cors(), userRouter);
