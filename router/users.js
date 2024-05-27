252d036ecc47b2218e46e93a632a3dce6a8c4518
Marina <likholetova.marina@gmail.com>
2024-05-27 14:40:49 +0300

    add error handling to user router

--- router/users.js
+++ router/users.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const { StatusCodes } = require("http-status-codes");
 const { userDataValidator, userIdValidator } = require("../middlewares/validators/userValidators");
+const { notFoundErrorHandler } = require("../middlewares/errorHandler");
 
 const userRouter = Router();
 
@@ -10,7 +11,7 @@ userRouter.get("/", (_req, resp) => {
 
 userRouter.get("/:userId", userIdValidator, (req, resp) => {
     resp.send(req.params);
-});
+}, notFoundErrorHandler);
 
 userRouter.post("/", userDataValidator, (_req, resp) => {
     resp.status(StatusCodes.CREATED).send("User created");
@@ -18,7 +19,11 @@ userRouter.post("/", userDataValidator, (_req, resp) => {
 
 userRouter.delete("/:userId", userIdValidator, (_req, resp) => {
     resp.status(StatusCodes.NO_CONTENT).end();
-});
+}, notFoundErrorHandler);
+
+userRouter.use((_req, resp) => {
+    resp.status(404).send();
+})
 
 module.exports = {
     userRouter,
